30035cbffe0122cdf6f4e9b40d822cc05207a5ed
shaoyoshita <59644539+shaoyoshita@users.noreply.github.com>
2025-07-16 05:19:55 +0200

    Create MainActivity.java

---
+++ java/com/zeno/core/MainActivity.java
@@ -0,0 +1,19 @@
+package com.zeno.core;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://zeno-core.netlify.app");
+        setContentView(webView);
+    }
+}
